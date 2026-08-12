0036a738d00d205a33745b4c88baf212cd326b17
AlirezaMohamadian1998 <alirezamohamadian1998@gmail.com>
2026-08-12 15:54:52 +0300

    feat(account): list authenticated customer accounts

--- src/main/java/com/neobank/neobank/account/AccountController.java
+++ src/main/java/com/neobank/neobank/account/AccountController.java
@@ -8,10 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -26,5 +25,10 @@ public class AccountController {
             @AuthenticationPrincipal Jwt jwt
     ) {
         return ResponseEntity.status(HttpStatus.CREATED).body(accountService.createAccount(request, jwt.getSubject()));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AccountResponse>> getCurrentCustomerAccounts(@AuthenticationPrincipal Jwt jwt) {
+        return ResponseEntity.ok(accountService.getCurrentCustomerAccounts(jwt.getSubject()));
     }
 }
--- src/main/java/com/neobank/neobank/account/AccountRepository.java
+++ src/main/java/com/neobank/neobank/account/AccountRepository.java
@@ -2,6 +2,10 @@ package com.neobank.neobank.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByAccountNumber(String accountNumber);
+
+    List<Account> findAllByCustomer_EmailIgnoreCaseOrderByCreatedAtDesc(String email);
 }
