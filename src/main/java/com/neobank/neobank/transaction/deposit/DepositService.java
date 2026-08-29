d7d258b451da30622965ca45627624ddf3ca6958
AlirezaMohamadian1998 <alirezamohamadian1998@gmail.com>
2026-08-29 17:59:42 +0300

    feat(deposits): support cross-currency deposits

--- src/main/java/com/neobank/neobank/transaction/deposit/DepositService.java
+++ src/main/java/com/neobank/neobank/transaction/deposit/DepositService.java
@@ -3,16 +3,16 @@ package com.neobank.neobank.transaction.deposit;
 import com.neobank.neobank.account.Account;
 import com.neobank.neobank.account.AccountNotFoundException;
 import com.neobank.neobank.account.AccountRepository;
+import com.neobank.neobank.fx.FxRateLockUnavailableException;
+import com.neobank.neobank.fx.FxRateService;
+import com.neobank.neobank.fx.dto.FxRateLockResponse;
 import com.neobank.neobank.idempotency.*;
 import com.neobank.neobank.internalaccount.InternalAccount;
 import com.neobank.neobank.internalaccount.InternalAccountPurpose;
 import com.neobank.neobank.internalaccount.InternalAccountRepository;
 import com.neobank.neobank.ledger.LedgerPostingService;
 import com.neobank.neobank.shared.reference.ReferenceGenerator;
-import com.neobank.neobank.transaction.BankTransaction;
-import com.neobank.neobank.transaction.BankTransactionRepository;
-import com.neobank.neobank.transaction.EntryDirection;
-import com.neobank.neobank.transaction.TransactionType;
+import com.neobank.neobank.transaction.*;
 import com.neobank.neobank.transaction.deposit.dto.DepositRequest;
 import com.neobank.neobank.transaction.deposit.dto.DepositResponse;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +21,9 @@ import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -41,6 +43,8 @@ public class DepositService {
 
     private final InternalAccountRepository internalAccountRepository;
 
+    private final FxRateService fxRateService;
+
     @Transactional
     @Retryable(maxRetries = 4,
             delay = 200,
@@ -58,15 +62,23 @@ public class DepositService {
             String customerEmail,
             String idempotencyKey
     ) {
-        if (!idempotencyKey.matches("[a-f0-9]{32}")) {
+        if(!idempotencyKey.matches("[a-f0-9]{32}")) {
             throw new InvalidIdempotencyKeyException("Invalid idempotency key");
         }
 
         Account account = accountRepository.findByAccountNumberAndCustomer_EmailIgnoreCase(accountNumber, customerEmail)
                 .orElseThrow(() -> new AccountNotFoundException());
 
+        if(account.getCurrency() != request.requestedCurrency() && request.lockId() == null) {
+            throw new FxRateLockUnavailableException("Lock ID is required for cross-currency operation.");
+        }
+
+        if(account.getCurrency() == request.requestedCurrency() && request.lockId() != null) {
+            throw new IllegalArgumentException("Lock ID is not required for same currency operation.");
+        }
+
         String normalizedNote = "";
-        if (request.note() != null) {
+        if(request.note() != null) {
             normalizedNote = request.note().trim();
         }
 
@@ -77,6 +89,9 @@ public class DepositService {
                 request.amount()
                         .setScale(2, RoundingMode.UNNECESSARY)
                         .toPlainString(),
+                request.requestedCurrency().name(),
+                account.getCurrency().name(),
+                request.lockId() != null ? request.lockId() : "",
                 normalizedNote
         );
 
@@ -88,7 +103,7 @@ public class DepositService {
                 requestHash
         );
 
-        if (existingIdempotencyRecord.isPresent()) {
+        if(existingIdempotencyRecord.isPresent()) {
             var existingTransaction = existingIdempotencyRecord.get().getBankTransaction();
 
             var existingEntry = existingTransaction
@@ -109,22 +124,63 @@ public class DepositService {
             );
         }
 
-        InternalAccount settlementAccount = internalAccountRepository.findByPurposeAndCurrency(InternalAccountPurpose.SETTLEMENT, account.getCurrency())
-                .orElseThrow(() -> new IllegalStateException("Settlement account is not configured for " + account.getCurrency()));
+        InternalAccount settlementAccount = internalAccountRepository.findByPurposeAndCurrency(InternalAccountPurpose.SETTLEMENT, request.requestedCurrency())
+                .orElseThrow(() -> new IllegalStateException("Settlement account is not configured for " + request.requestedCurrency()));
 
         BankTransaction bankTransaction = BankTransaction.createNew(
                 request.amount(),
-                account.getCurrency(),
+                request.requestedCurrency(),
                 TransactionType.DEPOSIT,
                 referenceGenerator.generate(),
                 request.note()
         );
+
+        FxInfo fxInfo = null;
+
+        if(account.getCurrency() != request.requestedCurrency() && request.lockId() != null) {
+            FxRateLockResponse cachedRate = fxRateService.getCachedRate(customerEmail, request.lockId());
+
+            if(cachedRate.baseCurrency() != request.requestedCurrency()) {
+                throw new FxRateLockUnavailableException("The base currency in the cached fx rate must be the same as the requested currency");
+            }
+
+            FxRate requestFxRate = FxRate.createNew(
+                    CurrencyContext.REQUEST,
+                    request.requestedCurrency(),
+                    cachedRate.rates().get(request.requestedCurrency())
+            );
+
+            FxRate sourceFxRate = FxRate.createNew(
+                    CurrencyContext.SOURCE,
+                    settlementAccount.getCurrency(),
+                    cachedRate.rates().get(settlementAccount.getCurrency())
+            );
+
+            FxRate destinationFxRate = FxRate.createNew(
+                    CurrencyContext.DESTINATION,
+                    account.getCurrency(),
+                    cachedRate.rates().get(account.getCurrency())
+            );
+
+            fxInfo = FxInfo.createNew(
+                    request.lockId(),
+                    Set.of(
+                            requestFxRate,
+                            sourceFxRate,
+                            destinationFxRate
+                    )
+            );
+
+            bankTransaction.addFxInfo(fxInfo);
+        }
 
         ledgerPostingService.post(
                 bankTransaction,
                 settlementAccount.getLedgerAccount(),
                 EntryDirection.DEBIT,
                 request.amount()
+                        .multiply(fxInfo != null ? fxInfo.getRate(CurrencyContext.SOURCE) : BigDecimal.ONE)
+                        .setScale(2, RoundingMode.HALF_EVEN)
         );
 
         var entry = ledgerPostingService.post(
@@ -132,6 +188,8 @@ public class DepositService {
                 account.getLedgerAccount(),
                 EntryDirection.CREDIT,
                 request.amount()
+                        .multiply(fxInfo != null ? fxInfo.getRate(CurrencyContext.DESTINATION) : BigDecimal.ONE)
+                        .setScale(2, RoundingMode.HALF_EVEN)
         );
 
         bankTransaction.complete();
